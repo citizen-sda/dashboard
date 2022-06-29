b92f510bd93a1ba6d11cbef778be5f3b1f27378d
Ananda Prasetyo <anandaprasetyo5@gmail.com>
2022-06-29 18:30:13 +0700

    menambahkan fitur search

--- pages/history.js
+++ pages/history.js
@@ -2,13 +2,13 @@ import React from 'react';
 import { FiArrowUpRight } from 'react-icons/fi';
 import Image from '../components/image';
 import toast from 'react-hot-toast';
+import NextImage from 'next/image';
+import { IoSearch } from 'react-icons/io5';
 
 // This gets called on every request
 export async function getServerSideProps() {
   // Fetch data from external API
-  const res = await fetch(
-    `https://k4b978r7ldkv68nte2cz.sycwell.xyz/api/report`
-  );
+  const res = await fetch(`${process.env.NEXT_PUBLIC_URL}/api/report`);
   const data = await res.json();
 
   // Pass data to the page via props
@@ -16,8 +16,13 @@ export async function getServerSideProps() {
 }
 
 export const History = ({ data }) => {
+  // State Untuk Menghapus Laporan
   const [history, setHistory] = React.useState(data.data);
+  // State Untuk Mengatur Pop Up
   const [isOpen, setIsOpen] = React.useState(false);
+  // State Untuk Mengambil dan Mengisi - Search: Menampilkan setSearch: Mengisi
+  const [search, setSearch] = React.useState('');
+
   // Update Status Laporan (verifikasi)
   const handleUpdate = async (id_report) => {
     const response = await fetch('/api/report', {
@@ -28,98 +33,121 @@ export const History = ({ data }) => {
         status: 'Ditinjau',
       }),
     });
+    // Hasil Respon API
     const result = await response.json();
     if (result) {
       setHistory(history.filter((item) => item.id_report != id_report));
+      // Menampilkan Toaster
       toast.success('Berhasil!');
     }
   };
 
   return (
-    // Card
-    <div className="grid sm:grid-cols-2 xl:grid-cols-4 grid-cols-1 gap-10">
-      {/* Filter Status Diverifikasi */}
-      {history
-        .filter((item) => item.status === 'Diverifikasi')
-        .map((item) => (
-          <div
-            key={item.id_report}
-            className="flex justify-center sm:justify-right"
-          >
-            <div className="card w-96 bg-base-100 shadow-xl">
-              {/* Gambar Laporan / Fasilitas */}
-              <Image src={item.image} className="w-96 h-60 object-cover" />
-              {/* Nama, status, dan deskripsi laporan */}
-              <div className="card-body">
-                <h2 className="card-title">
-                  {item.name}
-                  <div className="px-3 py-1 mt-1 bg-green-700 text-xs text-white rounded-xl">
-                    {item.status}
-                  </div>
-                </h2>
-                <p>{item.desc}</p>
+    <>
+      {/* Search Bar */}
+      <div className="relative mb-4 w-full">
+        <input
+          type="text"
+          placeholder="Search Report ..."
+          className="input input-bordered w-full mb-10 focus:outline-none"
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+        />
+        <IoSearch className="absolute right-4 top-[13px] text-xl text-gray-400" />
+      </div>
 
-                {/* Tombol Lihat Lokasi */}
-                <a
-                  className="flex items-center text-primary"
-                  href={`http://maps.google.com/maps?q=${item.latitude},${item.longitude}`}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  Lihat lokasi
-                  <FiArrowUpRight className="ml-1 mt-1" />
-                </a>
+      {/* Card */}
+      <div className="grid sm:grid-cols-2 xl:grid-cols-4 grid-cols-1 gap-10">
+        {/* Filter Status Diverifikasi */}
+        {history
+          .filter((item) => {
+            // Menyamakan hasil inputan yang telah ditampung dengan judul laporan yang telah difilter
+            if (search === '') {
+              return item.status === 'Diverifikasi';
+            } else if (item.name.toLowerCase().includes(search.toLowerCase())) {
+              return item.status === 'Diverifikasi';
+            }
+          })
+          .map((item) => (
+            <div
+              key={item.id_report}
+              className="flex justify-center sm:justify-right"
+            >
+              <div className="card w-96 bg-base-100 shadow-xl">
+                {/* Gambar laporan / gambar fasilitas */}
+                <Image src={item.image} className="w-96 h-60 object-cover" />
+                {/* Judul, status, dan deskripsi laporan */}
+                <div className="card-body">
+                  <h2 className="card-title">
+                    {item.name}
+                    <div className="px-3 py-1 mt-1 bg-green-700 text-xs text-white rounded-xl">
+                      {item.status}
+                    </div>
+                  </h2>
+                  <p>{item.desc}</p>
 
-                {/* Separator */}
-                <div className="border border-t border-black border-opacity-20 mt-4 mb-3" />
-                <div className="card-actions flex items-center justify-start mt-4">
-                  {/* Foto User */}
-                  <div className="w-10 sm:w-8">
-                    <img
-                      className="rounded-full"
-                      src={item.user.avatar}
-                      width="60"
-                      height="60"
-                    />
-                  </div>
-                  {/* Nama User */}
-                  <div>
-                    <p className="text-md">{item.user.name}</p>
-                    {/* Nama Badge */}
-                    <div className="flex items-center">
-                      <span className="">
-                        {item.user.badge[0].badgelist.name}
-                      </span>
-                      {/* Gambar Badge */}
-                      <div className="w-5 sm:w-5 ml-1">
-                        <img
-                          className="rounded-full"
-                          src={item.user.badge[0].badgelist.image}
-                          width="60"
-                          height="60"
-                        />
+                  {/* Tombol Lihat Lokasi */}
+                  <a
+                    className="flex items-center text-primary"
+                    href={`http://maps.google.com/maps?q=${item.latitude},${item.longitude}`}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    Lihat lokasi
+                    <FiArrowUpRight className="ml-1 mt-1" />
+                  </a>
+
+                  {/* Separator */}
+                  <div className="border border-t border-black border-opacity-20 mt-4 mb-3" />
+                  <div className="card-actions flex items-center justify-start mt-4">
+                    {/* Foto User */}
+                    <div className="w-10 sm:w-8">
+                      <NextImage
+                        className="rounded-full"
+                        src={item.user.avatar}
+                        width="60"
+                        height="60"
+                      />
+                    </div>
+                    {/* Nama User */}
+                    <div>
+                      <p className="text-md">{item.user.name}</p>
+                      {/* Nama Badge */}
+                      <div className="flex items-center">
+                        <span className="">
+                          {item.user.badge[0].badgelist.name}
+                        </span>
+                        {/* Gambar Badge */}
+                        <div className="w-5 sm:w-5 ml-1">
+                          <img
+                            className="rounded-full"
+                            src={item.user.badge[0].badgelist.image}
+                            width="60"
+                            height="60"
+                          />
+                        </div>
                       </div>
                     </div>
                   </div>
-                </div>
 
-                {/* Tombol Tinjau Ulang */}
-                <div className="grid mt-6">
-                  <button
-                    onClick={() => {
-                      setIsOpen(true);
-                      handleUpdate(item.id_report);
-                    }}
-                    className="px-6 py-2 text-white bg-primary hover:bg-gray-600 rounded-3xl"
-                  >
-                    Tinjau Ulang
-                  </button>
+                  {/* Tombol Tinjau Ulang */}
+                  <div className="grid mt-6">
+                    <button
+                      onClick={() => {
+                        setIsOpen(true);
+                        handleUpdate(item.id_report);
+                      }}
+                      className="px-6 py-2 border-0 text-white bg-primary hover:bg-gray-600 rounded-3xl"
+                    >
+                      Tinjau Ulang
+                    </button>
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
-        ))}
-    </div>
+          ))}
+      </div>
+    </>
   );
 };
 
--- pages/report.js
+++ pages/report.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { FiArrowUpRight } from 'react-icons/fi';
 import Image from '../components/image';
 import toast from 'react-hot-toast';
+import NextImage from 'next/image';
+import { IoSearch } from 'react-icons/io5';
 
 // This gets called on every request
 export async function getServerSideProps() {
@@ -14,9 +16,14 @@ export async function getServerSideProps() {
 }
 
 export const Report = ({ data }) => {
+  // State Untuk Menghapus Laporan
   const [report, setReport] = React.useState(data.data);
+  // State Untuk Mengatur Pop Up
   const [isOpen, setIsOpen] = React.useState(false);
-  // Update Status Laporan (verifikasi)
+  // State Untuk Mengambil dan Mengisi - Search: Menampilkan setSearch: Mengisi
+  const [search, setSearch] = React.useState('');
+
+  // Handle update status laporan (verifikasi)
   const handleUpdate = async (id_report) => {
     const response = await fetch('/api/report', {
       method: 'PUT',
@@ -26,14 +33,16 @@ export const Report = ({ data }) => {
         status: 'Diverifikasi',
       }),
     });
+    // Hasil Respon API
     const result = await response.json();
     if (result) {
       setReport(report.filter((item) => item.id_report != id_report));
+      // Menampilkan Toaster
       toast.success('Berhasil!');
     }
   };
 
-  // Hapus Laporan
+  // Handle hapus laporan
   const handleDelete = async (id_report) => {
     const response = await fetch('/api/report', {
       method: 'DELETE',
@@ -42,137 +51,162 @@ export const Report = ({ data }) => {
         id_report: id_report,
       }),
     });
+    // Hasil Respon API
     const result = await response.json();
     if (result) {
       setReport(report.filter((item) => item.id_report != id_report));
+      // Menampilkan Toaster
       toast.success('Terhapus');
     }
   };
 
   return (
-    // Card
-    <div className="grid sm:grid-cols-2 xl:grid-cols-4 grid-cols-1 gap-10">
-      {/* Filter Status Diverifikasi */}
-      {report
-        .filter((item) => item.status === 'Ditinjau')
-        .map((item) => (
-          <div
-            key={item.id_report}
-            className="flex justify-center sm:justify-right"
-          >
-            <div className="card w-96 bg-base-100 shadow-xl">
-              {/* Gambar Laporan / Fasilitas */}
-              <Image src={item.image} className="w-96 h-60 object-cover" />
-              {/* Nama, status, dan deskripsi laporan */}
-              <div className="card-body">
-                <h2 className="card-title">
-                  {item.name}
-                  <div className="px-3 py-1 mt-1 bg-primary text-xs text-white rounded-xl">
-                    {item.status}
+    <>
+      {/* Search Bar */}
+      <div className="relative mb-4 w-full">
+        <input
+          type="text"
+          placeholder="Search Report ..."
+          className="input input-bordered w-full mb-10 focus:outline-none"
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+        />
+        <IoSearch className="absolute right-4 top-[13px] text-xl text-gray-400" />
+      </div>
+
+      {/* Card */}
+      <div className="grid sm:grid-cols-2 xl:grid-cols-4 grid-cols-1 gap-10">
+        {/* Filter Status Diverifikasi */}
+        {report
+          .filter((item) => {
+            // Menyamakan hasil inputan yang telah ditampung dengan judul laporan yang telah difilter
+            if (search === '') {
+              return item.status === 'Ditinjau';
+            } else if (item.name.toLowerCase().includes(search.toLowerCase())) {
+              return item.status === 'Ditinjau';
+            }
+          })
+          .map((item) => (
+            <div
+              key={item.id_report}
+              className="flex justify-center sm:justify-right"
+            >
+              <div className="card w-96 bg-base-100 shadow-xl">
+                {/* Gambar Laporan / Fasilitas */}
+                <Image src={item.image} className="w-96 h-60 object-cover" />
+                {/* Nama, status, dan deskripsi laporan */}
+                <div className="card-body">
+                  <h2 className="card-title">
+                    {item.name}
+                    <div className="px-3 py-1 mt-1 bg-primary text-xs text-white rounded-xl">
+                      {item.status}
+                    </div>
+                  </h2>
+                  <p>{item.desc}</p>
+
+                  {/* Tombol Lihat Lokasi */}
+                  <a
+                    className="flex items-center text-primary"
+                    href={`http://maps.google.com/maps?q=${item.latitude},${item.longitude}`}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    Lihat lokasi
+                    <FiArrowUpRight className="ml-1 mt-1" />
+                  </a>
+
+                  {/* Separator */}
+                  <div className="border border-t border-black border-opacity-20 mt-4 mb-3" />
+                  <div className="card-actions flex items-center justify-start mt-4">
+                    {/* Foto User */}
+                    <div className="w-10 sm:w-8">
+                      <NextImage
+                        width={'100%'}
+                        height={'100%'}
+                        className="rounded-full"
+                        src={item.user.avatar}
+                      />
+                    </div>
+                    {/* Nama User */}
+                    <div>
+                      <p className="text-md">{item.user.name}</p>
+                      {/* Nama Badge */}
+                      <div className="flex items-center">
+                        <span className="">
+                          {item.user.badge[0].badgelist.name}
+                        </span>
+                        {/* Gambar Badge */}
+                        <div className="w-5 sm:w-5 ml-1">
+                          <img
+                            className="rounded-full"
+                            src={item.user.badge[0].badgelist.image}
+                            width="60"
+                            height="60"
+                          />
+                        </div>
+                      </div>
+                    </div>
                   </div>
-                </h2>
-                <p>{item.desc}</p>
 
-                {/* Tombol Lihat Lokasi */}
-                <a
-                  className="flex items-center text-primary"
-                  href={`http://maps.google.com/maps?q=${item.latitude},${item.longitude}`}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  Lihat lokasi
-                  <FiArrowUpRight className="ml-1 mt-1" />
-                </a>
+                  {/* Tombol Respon */}
+                  <div className="grid sm:grid-cols-2 grid-cols-1 gap-2 mt-6">
+                    {/* Tombol Hapus */}
+                    <button
+                      onClick={() => setIsOpen(true)}
+                      className="btn modal-button px-6 py-2 border-0 text-white bg-red-700 hover:bg-red-500 rounded-3xl capitalize"
+                    >
+                      Hapus
+                    </button>
 
-                {/* Separator */}
-                <div className="border border-t border-black border-opacity-20 mt-4 mb-3" />
-                <div className="card-actions flex items-center justify-start mt-4">
-                  {/* Foto User */}
-                  <div className="w-10 sm:w-8">
-                    <img
-                      className="rounded-full"
-                      src={item.user.avatar}
-                      width="60"
-                      height="60"
+                    {/* Pop Up Modal */}
+                    <input
+                      type="checkbox"
+                      id="my-modal"
+                      className="modal-toggle"
                     />
-                  </div>
-                  {/* Nama User */}
-                  <div>
-                    <p className="text-md">{item.user.name}</p>
-                    {/* Nama Badge */}
-                    <div className="flex items-center">
-                      <span className="">
-                        {item.user.badge[0].badgelist.name}
-                      </span>
-                      {/* Gambar Badge */}
-                      <div className="w-5 sm:w-5 ml-1">
-                        <img
-                          className="rounded-full"
-                          src={item.user.badge[0].badgelist.image}
-                          width="60"
-                          height="60"
-                        />
+                    <div className={`${isOpen ? 'modal-open' : ''} modal`}>
+                      <div className="modal-box">
+                        <h3 className="font-bold text-lg">Hapus laporan?</h3>
+                        <p className="py-4">
+                          Pastikan telah memeriksa laporan dengan teliti, karena
+                          laporan yang terhapus tidak akan bisa dikembalikan.
+                        </p>
+                        <div className="modal-action">
+                          {/* Tombol Ya */}
+                          <button
+                            onClick={() => {
+                              handleDelete(item.id_report);
+                              setIsOpen(false);
+                            }}
+                            className="btn border-0 text-white bg-green-700 hover:bg-green-500"
+                          >
+                            Ya
+                          </button>
+                          {/* Tombol Batal */}
+                          <button
+                            onClick={() => setIsOpen(false)}
+                            className="btn border-0 text-white bg-red-700 hover:bg-red-500"
+                          >
+                            Batal
+                          </button>
+                        </div>
                       </div>
                     </div>
+
+                    {/* Tombol Verifikasi */}
+                    <button
+                      onClick={() => handleUpdate(item.id_report)}
+                      className="px-3 py-2 border-0 text-white bg-green-700 hover:bg-green-500 rounded-3xl"
+                    >
+                      Verifikasi
+                    </button>
                   </div>
-                </div>
-
-                {/* Tombol Respon */}
-                <div className="grid sm:grid-cols-2 grid-cols-1 gap-2 mt-6">
-                  {/* Tombol Hapus */}
-                  <button
-                    onClick={() => setIsOpen(true)}
-                    className="btn modal-button px-6 py-2 text-white bg-red-700 hover:bg-red-500 rounded-3xl capitalize"
-                  >
-                    Hapus
-                  </button>
-
-                  {/* Pop Up Modal */}
-                  <input
-                    type="checkbox"
-                    id="my-modal"
-                    className="modal-toggle"
-                  />
-                  <div className={`${isOpen ? 'modal-open' : ''} modal`}>
-                    <div className="modal-box">
-                      <h3 className="font-bold text-lg">Hapus laporan?</h3>
-                      <p className="py-4">
-                        Pastikan telah memeriksa laporan dengan teliti, karena
-                        laporan yang terhapus tidak akan bisa dikembalikan.
-                      </p>
-                      <div className="modal-action">
-                        <button
-                          onClick={() => {
-                            handleDelete(item.id_report);
-                            setIsOpen(false);
-                          }}
-                          className="btn"
-                        >
-                          YA
-                        </button>
-                        <button
-                          onClick={() => setIsOpen(false)}
-                          className="btn btn-error"
-                        >
-                          Cancel
-                        </button>
-                      </div>
-                    </div>
-                  </div>
-
-                  {/* Tombol Verifikasi */}
-                  <button
-                    onClick={() => handleUpdate(item.id_report)}
-                    className="px-3 py-2 text-white bg-green-700 hover:bg-green-500 rounded-3xl"
-                  >
-                    Verifikasi
-                  </button>
                 </div>
               </div>
             </div>
-          </div>
-        ))}
-    </div>
+          ))}
+      </div>
+    </>
   );
 };
 
