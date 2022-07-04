c85cd2ec265e6cef5882f7d39972d8053c0949e3
Ananda Prasetyo <anandaprasetyo5@gmail.com>
2022-07-04 19:11:02 +0700

    memfungsikan crud pada halaman badge

--- pages/api/badge/index.js
+++ pages/api/badge/index.js
@@ -1,10 +1,39 @@
 import supabase from '../../../lib/supabase';
 
 export default async function handler(req, res) {
-  const { data } = await supabase
-    .from('badge_list')
-    .select('*')
-    .order('id_badgelist', { ascending: false });
+  if (req.method === 'POST') {
+    const { error } = await supabase.from('badge_list').insert({
+      id_badgelist: req.body.id_badgelist,
+      name: req.body.name,
+      image: req.body.image,
+      description: req.body.description,
+      requirement: req.body.requirement,
+    });
 
-  res.status(200).json(data);
+    res.status(200).json({ error });
+  } else if (req.method === 'PUT') {
+    const { error } = await supabase
+      .from('badge_list')
+      .update({
+        name: req.body.name,
+        image: req.body.image,
+        description: req.body.description,
+        requirement: req.body.requirement,
+      })
+      .match({ id_badgelist: req.body.id_badgelist });
+    res.status(200).json({ error });
+  } else if (req.method === 'DELETE') {
+    const { error } = await supabase
+      .from('badge_list')
+      .delete()
+      .match({ id_badgelist: req.body.id_badgelist });
+    res.status(200).json({ error });
+  } else {
+    const { data } = await supabase
+      .from('badge_list')
+      .select('*')
+      .order('id_badgelist', { ascending: false });
+
+    res.status(200).json(data);
+  }
 }
--- pages/badge.js
+++ pages/badge.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { IoMdAdd } from 'react-icons/io';
+import { useForm } from 'react-hook-form';
+import toast from 'react-hot-toast';
 
 // This gets called on every request
 export async function getServerSideProps() {
@@ -12,51 +14,298 @@ export async function getServerSideProps() {
 }
 
 export const Badge = ({ data }) => {
-  // State Untuk Mengatur Pop Up
-  const [isOpen, setIsOpen] = React.useState(false);
+  const [badge, setBadge] = React.useState(data);
+  // State Untuk Mengatur Pop Up Modal
+  const [isOpenAdd, setIsOpenAdd] = React.useState(false);
+  const [isOpenEdit, setIsOpenEdit] = React.useState(false);
+  const [isOpenDelete, setIsOpenDelete] = React.useState(false);
+  const [selectedItem, setSelectedItem] = React.useState({
+    id_badgelist: '',
+    name: '',
+    description: '',
+    requirement: '',
+    image: '',
+  });
+
+  //for add
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmitAdd = (data) => {
+    handlePost(data);
+  };
+
+  const handleAfter = async () => {
+    const response = await fetch('/api/badge', {
+      method: 'GET',
+    });
+
+    // Hasil Respon API
+    const result = await response.json();
+    setBadge(result);
+  };
+
+  // Handle hapus badge
+  const handleDelete = async (id_badgelist) => {
+    const response = await fetch('/api/badge', {
+      method: 'DELETE',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        id_badgelist: id_badgelist,
+      }),
+    });
+
+    // Hasil Respon API
+    const result = await response.json();
+    if (result) {
+      handleAfter();
+      // Menampilkan Toaster
+      toast.success('Terhapus');
+    }
+  };
+
+  // Handle post badge
+  const handlePost = async (data) => {
+    const { id_badgelist, name, description, requirement, image } = data;
+    const response = await fetch('/api/badge', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        id_badgelist,
+        name,
+        description,
+        requirement,
+        image,
+      }),
+    });
+
+    // Hasil Respon API
+    const result = await response.json();
+    if (result.error == null) {
+      console.log(result);
+      handleAfter();
+      // Menampilkan Toaster
+      toast.success('Berhasil!');
+      setIsOpenAdd(false);
+      reset({
+        id_badgelist: '',
+        name: '',
+        description: '',
+        requirement: '',
+        image: '',
+      });
+    }
+  };
+
+  // Handle edit / update badge
+  const handleEdit = async (data) => {
+    const response = await fetch('/api/badge', {
+      method: 'PUT',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(data),
+    });
+
+    // Hasil Respon API
+    const result = await response.json();
+    if (result.error == null) {
+      handleAfter();
+      // Menampilkan Toaster
+      toast.success('Berhasil!');
+      setIsOpenEdit(false);
+    }
+  };
+
   return (
     <>
-      {/* Pop Up Modal */}
+      {/* Pop Up Modal Add*/}
       <input type="checkbox" id="my-modal" className="modal-toggle" />
-      <div className={`${isOpen ? 'modal-open' : ''} modal`}>
+      <div className={`${isOpenAdd ? 'modal-open' : ''} modal`}>
         <div className="modal-box">
-          <p className="text-md">ID Badge</p>
-          <input
-            type="text"
-            placeholder="20001"
-            className="input input-bordered w-full mt-1 focus:outline-none"
-          />
-          <p className="text-md mt-4">Name</p>
-          <input
-            type="text"
-            placeholder="Trial"
-            className="input input-bordered w-full mt-1 focus:outline-none"
-          />
-          <p className="text-md mt-4">Description</p>
-          <input
-            type="text"
-            placeholder="User yang hebat"
-            className="input input-bordered w-full mt-1 focus:outline-none"
-          />
-          <p className="text-md mt-4">Requirement</p>
-          <input
-            type="text"
-            placeholder="10"
-            className="input input-bordered w-full mt-1 focus:outline-none"
-          />
-          <p className="text-md mt-4">Image</p>
-          <input
-            type="text"
-            placeholder="https://raw.githubusercontent.com/"
-            className="input input-bordered w-full mt-1 focus:outline-none"
-          />
+          <form onSubmit={handleSubmit(onSubmitAdd)}>
+            <p className="text-md">ID Badge</p>
+            <input
+              {...register('id_badgelist', { required: true })}
+              type="text"
+              placeholder="20001"
+              className="input input-bordered w-full mt-1 focus:outline-none"
+            />
+            {errors.name && (
+              <span className="text-red-600">This field is required</span>
+            )}
+            <p className="text-md mt-4">Name</p>
+            <input
+              {...register('name', { required: true })}
+              type="text"
+              placeholder="Trial"
+              className="input input-bordered w-full mt-1 focus:outline-none"
+            />
+            {errors.name && (
+              <span className="text-red-600">This field is required</span>
+            )}
+            <p className="text-md mt-4">Description</p>
+            <input
+              {...register('description', { required: true })}
+              type="text"
+              placeholder="User yang hebat"
+              className="input input-bordered w-full mt-1 focus:outline-none"
+            />
+            {errors.latitude && (
+              <span className="text-red-600">This field is required</span>
+            )}
+            <p className="text-md mt-4">Requirement</p>
+            <input
+              {...register('requirement', { required: true })}
+              type="text"
+              placeholder="10"
+              className="input input-bordered w-full mt-1 focus:outline-none"
+            />
+            {errors.latitude && (
+              <span className="text-red-600">This field is required</span>
+            )}
+            <p className="text-md mt-4">Image</p>
+            <input
+              {...register('image', { required: true })}
+              type="text"
+              placeholder="https://raw.githubusercontent.com/"
+              className="input input-bordered w-full mt-1 focus:outline-none"
+            />
+            {errors.longitude && (
+              <span className="text-red-600">This field is required</span>
+            )}
 
+            <div className="modal-action">
+              {/* Tombol Ya */}
+              <input
+                type="submit"
+                className="btn border-0 text-white bg-green-700 hover:bg-green-500"
+              />
+              {/* Tombol Batal */}
+              <button
+                onClick={() => setIsOpenAdd(false)}
+                className="btn border-0 text-white bg-red-700 hover:bg-red-500"
+              >
+                Batal
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
+
+      {/* Pop Up Modal Edit*/}
+      <input type="checkbox" id="my-modal" className="modal-toggle" />
+      <div className={`${isOpenEdit ? 'modal-open' : ''} modal`}>
+        <div className="modal-box">
+          <form
+            onSubmit={(e) => {
+              e.preventDefault();
+              handleEdit(selectedItem);
+            }}
+          >
+            <p className="text-md">ID Badge</p>
+            <input
+              value={selectedItem.id_badgelist}
+              type="text"
+              required
+              onChange={(e) =>
+                setSelectedItem({
+                  ...selectedItem,
+                  id_badgelist: e.target.value,
+                })
+              }
+              className="input input-bordered w-full mt-1 focus:outline-none"
+            />
+
+            <p className="text-md mt-4">Name</p>
+            <input
+              value={selectedItem.name}
+              type="text"
+              required
+              onChange={(e) =>
+                setSelectedItem({
+                  ...selectedItem,
+                  name: e.target.value,
+                })
+              }
+              className="input input-bordered w-full mt-1 focus:outline-none"
+            />
+            <p className="text-md mt-4">Description</p>
+            <input
+              value={selectedItem.description}
+              type="text"
+              required
+              onChange={(e) =>
+                setSelectedItem({
+                  ...selectedItem,
+                  descriptione: e.target.value,
+                })
+              }
+              className="input input-bordered w-full mt-1 focus:outline-none"
+            />
+            <p className="text-md mt-4">Requirement</p>
+            <input
+              value={selectedItem.requirement}
+              type="text"
+              required
+              onChange={(e) =>
+                setSelectedItem({
+                  ...selectedItem,
+                  requirement: e.target.value,
+                })
+              }
+              className="input input-bordered w-full mt-1 focus:outline-none"
+            />
+
+            <p className="text-md mt-4">Image</p>
+            <input
+              value={selectedItem.image}
+              type="text"
+              required
+              onChange={(e) =>
+                setSelectedItem({
+                  ...selectedItem,
+                  image: e.target.value,
+                })
+              }
+              className="input input-bordered w-full mt-1 focus:outline-none"
+            />
+
+            <div className="modal-action">
+              {/* Tombol Ya */}
+              <input
+                type="submit"
+                className="btn border-0 text-white bg-green-700 hover:bg-green-500"
+              />
+              {/* Tombol Batal */}
+              <button
+                onClick={() => setIsOpenEdit(false)}
+                className="btn border-0 text-white bg-red-700 hover:bg-red-500"
+              >
+                Batal
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
+
+      {/* Pop Up Modal Delete */}
+      <input type="checkbox" id="my-modal" className="modal-toggle" />
+      <div className={`${isOpenDelete ? 'modal-open' : ''} modal`}>
+        <div className="modal-box">
+          <h3 className="font-bold text-lg">Hapus laporan?</h3>
+          <p className="py-4">
+            Pastikan telah memeriksa laporan dengan teliti, karena laporan yang
+            terhapus tidak akan bisa dikembalikan.
+          </p>
           <div className="modal-action">
             {/* Tombol Ya */}
             <button
               onClick={() => {
-                handleDelete(item.id_report);
-                setIsOpen(false);
+                handleDelete(selectedItem.id_badgelist);
+                setIsOpenDelete(false);
               }}
               className="btn border-0 text-white bg-green-700 hover:bg-green-500"
             >
@@ -64,7 +313,7 @@ export const Badge = ({ data }) => {
             </button>
             {/* Tombol Batal */}
             <button
-              onClick={() => setIsOpen(false)}
+              onClick={() => setIsOpenDelete(false)}
               className="btn border-0 text-white bg-red-700 hover:bg-red-500"
             >
               Batal
@@ -85,7 +334,7 @@ export const Badge = ({ data }) => {
               <th>Requirement</th>
               <th>Image</th>
               <th className="flex justify-end">
-                <button onClick={() => setIsOpen(true)}>
+                <button onClick={() => setIsOpenAdd(true)}>
                   <IoMdAdd className="text-2xl" />
                 </button>
               </th>
@@ -93,7 +342,7 @@ export const Badge = ({ data }) => {
           </thead>
           <tbody>
             {/* <!-- row 1 --> */}
-            {data.map((item) => (
+            {badge.map((item) => (
               <tr key={item.id_badgelist}>
                 <th>{item.id_badgelist}</th>
                 <th>{item.name}</th>
@@ -101,10 +350,37 @@ export const Badge = ({ data }) => {
                 <td>{item.requirement}</td>
                 <td>{item.image}</td>
                 <td>
-                  <button className="btn btn-ghost btn-md text-white bg-green-700 m-1">
+                  {/* Tombol Edit */}
+                  <button
+                    onClick={() => {
+                      setIsOpenEdit(true);
+                      setSelectedItem({
+                        id_badgelist: item.id_badgelist,
+                        name: item.name,
+                        description: item.description,
+                        requirement: item.requirement,
+                        image: item.image,
+                      });
+                    }}
+                    className="btn btn-ghost btn-md text-white bg-green-700 m-1"
+                  >
                     edit
                   </button>
-                  <button className="btn btn-ghost btn-md text-white bg-red-700 m-1">
+
+                  {/* Tombol Delete */}
+                  <button
+                    onClick={() => {
+                      setIsOpenDelete(true);
+                      setSelectedItem({
+                        id_badgelist: item.id_badgelist,
+                        name: item.name,
+                        description: item.description,
+                        requirement: item.requirement,
+                        image: item.image,
+                      });
+                    }}
+                    className="btn btn-ghost btn-md text-white bg-red-700 m-1"
+                  >
                     delete
                   </button>
                 </td>
